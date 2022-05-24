8b7f1d20fdbdfa3e9a48be54d034ac4ffe9b77cd
akrapovicivan <iakrap00@fesb.hr>
2022-05-24 10:46:34 +0200

    updated user

--- wordApiProject/User.cs
+++ wordApiProject/User.cs
@@ -1,18 +1,27 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace wordApiProject
 {
     public class User
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; } = string.Empty;
+        [Required]
         public string LastName { get; set; } = string.Empty;
+        [Required]
+        public string Nickname { get; set; } = string.Empty;
+        [Required]
         public string Email { get; set; } = string.Empty;
         public string BussinessMail { get; set; } = string.Empty;
         public string DateOfBirth { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
+
         [ForeignKey("Has")]
         int HasID { get; set; }
-
+        
     }
 }
