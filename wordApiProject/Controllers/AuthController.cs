4f4df3dfb26ef33d3477bcc75f41dc0992045426
Ivor Gradinski <ivorgradinski@gmail.com>
2022-06-01 12:10:17 +0200

    Login completed

--- wordApiProject/Controllers/AuthController.cs
+++ wordApiProject/Controllers/AuthController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -12,16 +13,39 @@ namespace wordApiProject.Controllers
     [Route("api/auth")]
     [ApiController]
     public class AuthController : ControllerBase
+
+
     {
+        private readonly DataContext _context;
+
+        public AuthController(DataContext context)
+        {
+            _context = context;
+        }
         [HttpPost,Route ("login")]
 
         public IActionResult Login([FromBody] LoginModel LoginUser)
         {
 
-            if (LoginUser == null)
-                return BadRequest("invalid client request");
+            //if (LoginUser == null)
+            //  return BadRequest("invalid client request");
 
-            if (LoginUser.UserName=="doktor@test.com" && LoginUser.Password=="ja")
+            //if (LoginUser.UserName=="doktor@test.com" && LoginUser.Password=="ja")
+            var checkpassword = string.Empty;
+            var check = (from User in _context.Users where _context.Users.Any(x=>x.Email == LoginUser.UserName)select User).ToList();
+            
+            foreach(User user in check)
+            {
+                checkpassword = user.Password;
+                
+            }
+            
+            Console.WriteLine(LoginUser.UserName);
+            Console.WriteLine(LoginUser.Password);
+            if (check.Count == 0 || LoginUser.Password!=checkpassword) {
+                return BadRequest("invalid client request");
+            } else
+
             {
                 var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("this is my custom Secret key for authentication@345"));
                 var signingCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
@@ -37,7 +61,7 @@ namespace wordApiProject.Controllers
                 var tokenString = new JwtSecurityTokenHandler().WriteToken(tokeOptions);
                 return Ok(new { Token = tokenString });
             }
-            return Unauthorized();
+            //return Unauthorized();
         }
     }
 }
