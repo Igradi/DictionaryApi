6807b1457d466d7a2ca1de6e4c94a9e33d7177b3
akrapovicivan <iakrap00@fesb.hr>
2022-06-08 11:11:29 +0200

    working login

--- wordApiProject/Controllers/AuthController.cs
+++ wordApiProject/Controllers/AuthController.cs
@@ -7,41 +7,38 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using wordApiProject.Models;
-
 namespace wordApiProject.Controllers
 {
     [Route("api/auth")]
     [ApiController]
     public class AuthController : ControllerBase
-
-
     {
         private readonly DataContext _context;
-
         public AuthController(DataContext context)
         {
             _context = context;
         }
-        [HttpPost,Route ("login")]
-
+        [HttpPost, Route("login")]
         public IActionResult Login([FromBody] LoginModel LoginUser)
         {
-
-            
             var checkpassword = string.Empty;
-            var check = (from User in _context.Users where _context.Users.Any(x=>x.Email == LoginUser.UserName)select User).ToList();
-            
-            foreach(User user in check)
+            var primaryKey = from User in _context.Users where (User.Email == LoginUser.UserName) select User.Id;
+            int id = primaryKey.First();
+            var check = _context.Users.Find(id);
+
+            if (check == null)
             {
-                checkpassword = user.Password;
-                
+                return BadRequest("invalid client request");
             }
+                checkpassword = check.Password;
+            
             //Console.WriteLine(LoginUser.UserName);
             //Console.WriteLine(LoginUser.Password);
-            if (check.Count == 0 || LoginUser.Password!=checkpassword) {
+            if(check.Password != checkpassword)
+            {
                 return BadRequest("invalid client request");
-            } else
-
+            }
+            else
             {
                 var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("this is my custom Secret key for authentication@345"));
                 var signingCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
@@ -49,7 +46,7 @@ namespace wordApiProject.Controllers
                     issuer: "https://localhost:7153",
                     audience: "https://localhost:7153",
                     claims: new List<Claim>(),
-                    expires:DateTime.Now.AddMinutes(5),
+                    expires: DateTime.Now.AddMinutes(5),
                     signingCredentials: signingCredentials
                     );
                 var tokenString = new JwtSecurityTokenHandler().WriteToken(tokeOptions);
