4ac2c30785980a97a5650dce5e3ff88b029c6a5a
Ivor Gradinski <ivorgradinski@gmail.com>
2022-06-08 11:36:07 +0200

    login fix

--- wordApiProject/Controllers/AuthController.cs
+++ wordApiProject/Controllers/AuthController.cs
@@ -7,55 +7,48 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using wordApiProject.Models;
-
 namespace wordApiProject.Controllers
 {
     [Route("api/auth")]
     [ApiController]
     public class AuthController : ControllerBase
-
-
     {
         private readonly DataContext _context;
-
         public AuthController(DataContext context)
         {
             _context = context;
         }
-        [HttpPost,Route ("login")]
-
+        [HttpPost, Route("login")]
         public IActionResult Login([FromBody] LoginModel LoginUser)
         {
-
-            
-            var checkpassword = string.Empty;
-            var check = (from User in _context.Users where _context.Users.Any(x=>x.Email == LoginUser.UserName)select User).ToList();
-            
-            foreach(User user in check)
+            try
             {
-                checkpassword = user.Password;
-                
+                var primaryKey = from User in _context.Users where (User.Email == LoginUser.UserName) select User.Id;
+                int id = primaryKey.First();
+                var check = _context.Users.Find(id);
+                if (check.Password != LoginUser.Password)
+                {
+                    return BadRequest("Wrong password");
+                }
+                else
+                {
+                    var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("this is my custom Secret key for authentication@345"));
+                    var signingCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+                    var tokeOptions = new JwtSecurityToken(
+                        issuer: "https://localhost:7153",
+                        audience: "https://localhost:7153",
+                        claims: new List<Claim>(),
+                        expires: DateTime.Now.AddMinutes(5),
+                        signingCredentials: signingCredentials
+                        );
+                    var tokenString = new JwtSecurityTokenHandler().WriteToken(tokeOptions);
+                    return Ok(new { Token = tokenString });
+                }
             }
-            //Console.WriteLine(LoginUser.UserName);
-            //Console.WriteLine(LoginUser.Password);
-            if (check.Count == 0 || LoginUser.Password!=checkpassword) {
-                return BadRequest("invalid client request");
-            } else
-
+            catch
             {
-                var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("this is my custom Secret key for authentication@345"));
-                var signingCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
-                var tokeOptions = new JwtSecurityToken(
-                    issuer: "https://localhost:7153",
-                    audience: "https://localhost:7153",
-                    claims: new List<Claim>(),
-                    expires:DateTime.Now.AddMinutes(5),
-                    signingCredentials: signingCredentials
-                    );
-                var tokenString = new JwtSecurityTokenHandler().WriteToken(tokeOptions);
-                return Ok(new { Token = tokenString });
+                return BadRequest("User not found");
             }
-            //return Unauthorized();
         }
     }
 }
