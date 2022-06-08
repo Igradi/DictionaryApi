b718b188d058591725509e8376f7bade12a24cfd
akrapovicivan <iakrap00@fesb.hr>
2022-06-08 11:35:57 +0200

    Working login

--- wordApiProject/Controllers/AuthController.cs
+++ wordApiProject/Controllers/AuthController.cs
@@ -21,38 +21,33 @@ namespace wordApiProject.Controllers
         [HttpPost, Route("login")]
         public IActionResult Login([FromBody] LoginModel LoginUser)
         {
-            var checkpassword = string.Empty;
-            var primaryKey = from User in _context.Users where (User.Email == LoginUser.UserName) select User.Id;
-            int id = primaryKey.First();
-            var check = _context.Users.Find(id);
-
-            if (check == null)
-            {
-                return BadRequest("invalid client request");
+            try { 
+                var primaryKey = from User in _context.Users where (User.Email == LoginUser.UserName) select User.Id;
+                int id = primaryKey.First();
+                var check = _context.Users.Find(id);
+                if (check.Password != LoginUser.Password)
+                {
+                    return BadRequest("Wrong password");
+                }
+                else
+                {
+                    var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("this is my custom Secret key for authentication@345"));
+                    var signingCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+                    var tokeOptions = new JwtSecurityToken(
+                        issuer: "https://localhost:7153",
+                        audience: "https://localhost:7153",
+                        claims: new List<Claim>(),
+                        expires: DateTime.Now.AddMinutes(5),
+                        signingCredentials: signingCredentials
+                        );
+                    var tokenString = new JwtSecurityTokenHandler().WriteToken(tokeOptions);
+                    return Ok(new { Token = tokenString });
+                }
             }
-                checkpassword = check.Password;
-            
-            //Console.WriteLine(LoginUser.UserName);
-            //Console.WriteLine(LoginUser.Password);
-            if(check.Password != checkpassword)
+            catch
             {
-                return BadRequest("invalid client request");
+                return BadRequest("User not found");
             }
-            else
-            {
-                var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("this is my custom Secret key for authentication@345"));
-                var signingCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
-                var tokeOptions = new JwtSecurityToken(
-                    issuer: "https://localhost:7153",
-                    audience: "https://localhost:7153",
-                    claims: new List<Claim>(),
-                    expires: DateTime.Now.AddMinutes(5),
-                    signingCredentials: signingCredentials
-                    );
-                var tokenString = new JwtSecurityTokenHandler().WriteToken(tokeOptions);
-                return Ok(new { Token = tokenString });
-            }
-            //return Unauthorized();
         }
     }
 }
