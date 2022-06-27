bf68a7e56fd831f88f23f002dff9745ac6e01240
akrapovicivan <iakrap00@fesb.hr>
2022-06-27 10:20:18 +0200

    working admin (delete in hass controller)

--- wordApiProject/Controllers/HasController.cs
+++ wordApiProject/Controllers/HasController.cs
@@ -21,7 +21,34 @@ namespace wordApiProject.Controllers
 
             yield return await targetWords;
         }
-
+        [HttpDelete("id")]
+        public async Task<ActionResult<int>> Delete(int id)
+        {
+            var user = _context.Users.Find(id);
+            Console.WriteLine(user.Name);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            else
+            _context.Users.Remove(user);
+            _context.SaveChanges();
+            var wordsId = (from Has in _context.Hass where Has.UserId == id select Has.WordId).ToList();
+            var hassIds = from Has in _context.Hass where Has.UserId == id select Has;
+            foreach(Has hass in hassIds)
+            {
+                _context.Hass.Remove(hass);
+            }
+            _context.SaveChanges();
+            var words = from Words in _context.Words where wordsId.Contains(Words.Id) select Words;
+            
+            foreach (Words word in words)
+            {
+                _context.Words.Remove(word);
+            }
+            _context.SaveChanges();
+            return Ok(id);
+        }
     }
     }
 
