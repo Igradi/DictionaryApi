2e1a4ad3955f4c2b16ca31ed7ca1738a1bd942a8
Ivor <ivorgradinski@gmail.com>
2022-07-05 19:26:49 +0200

    mail

--- wordApiProject/Controllers/EmailController.cs
+++ wordApiProject/Controllers/EmailController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MimeKit;
 using MimeKit.Text;
+using wordApiProject.Models;
 
 namespace wordApiProject.Controllers
 {
@@ -14,11 +15,12 @@ namespace wordApiProject.Controllers
         [HttpPost]
         public IActionResult SendEmail(string body)
         {
+            var resetUser = new ResetPasswordRequest();
             var email = new MimeMessage();
             email.From.Add(MailboxAddress.Parse("aglae.hackett58@ethereal.email"));
             email.To.Add(MailboxAddress.Parse("aglae.hackett58@ethereal.email"));
             email.Subject = "test mail subject";
-            email.Body=new TextPart(TextFormat.Html) { Text=body };
+            email.Body=new TextPart(TextFormat.Html) { Text=resetUser.Token };
 
             using var smtp = new SmtpClient();
             smtp.Connect("smtp.ethereal.email", 587,SecureSocketOptions.StartTls);
