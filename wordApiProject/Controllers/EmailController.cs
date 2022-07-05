86797676eaa927322c67d53cd1294e68f5d070ce
Ivor <ivorgradinski@gmail.com>
2022-07-05 12:24:31 +0200

    Email Controller

---
+++ wordApiProject/Controllers/EmailController.cs
@@ -0,0 +1,32 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MimeKit.Text;
+
+namespace wordApiProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult SendEmail(string body)
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse("aglae.hackett58@ethereal.email"));
+            email.To.Add(MailboxAddress.Parse("aglae.hackett58@ethereal.email"));
+            email.Subject = "test mail subject";
+            email.Body=new TextPart(TextFormat.Html) { Text=body };
+
+            using var smtp = new SmtpClient();
+            smtp.Connect("smtp.ethereal.email", 587,SecureSocketOptions.StartTls);
+            smtp.Authenticate("aglae.hackett58@ethereal.email", "tJ6m3h6BXhAdzJ9YuF");
+            smtp.Send(email);
+            smtp.Disconnect(true);
+
+            return Ok();
+        }
+    }
+}
